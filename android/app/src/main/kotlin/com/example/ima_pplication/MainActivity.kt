78f5817a1b7b4559ab64a881d59a18db05a81250
RCharlotte <ruthcharlotte02@gmail.com>
2023-09-12 11:54:22 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ima_pplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ima_pplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/frontend/widgets/custom_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({Key? key, required this.title, required this.onPressed, required this.style})
+      : super(key: key);
+  final String title;
+  final VoidCallback onPressed;
+  final ButtonStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextButton(
+        onPressed: onPressed,
+        style: style ,
+        child: Text(title),
+      ),
+    );
+  }
+}
